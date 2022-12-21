3e9a4feeb3c7cd5b05d474e6288c6d2fafabdce6
ondrej <ondrej@sanctusmedia.com>
2022-12-21 16:38:00 +0100

    Initial Commit

---
+++ GreetingsApp/GreetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsAppApp.swift
+//  GreetingsApp
+//
+//  Created by Ondrej on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
